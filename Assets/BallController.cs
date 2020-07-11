c44d04198edf25e138c0286be3b2dbec1be511e7
Brandt <starshipbrandt@gmail.com>
2020-07-11 17:22:55 -0500

    Added Auto Ball flag and Launch() method

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,17 +5,26 @@ using UnityEngine.InputSystem;
 
 public class BallController : MonoBehaviour
 {
+    public bool autoBall = false;
     public Rigidbody rb;
     public float speed = 9.0f;
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = speed * new Vector3(0.0f, 1.0f, 1.0f);
+        rb.isKinematic = true;
+        if (autoBall) Launch(speed * new Vector3(0.0f, 1.0f, 1.0f));
+        //rb.velocity = speed * new Vector3(0.0f, 1.0f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Launch(Vector3 velocity)
+    {
+        rb.isKinematic = false;
+        rb.velocity = velocity;
     }
 }
