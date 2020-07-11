49d39192930add4c0be34f0063bb1f4e9f94a8ef
Brandt <starshipbrandt@gmail.com>
2020-07-11 02:52:15 -0500

    Colliding with backstop

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,10 +5,12 @@ using UnityEngine.InputSystem;
 
 public class BallController : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float speed = 9.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = speed * new Vector3(0.0f, 1.0f, 1.0f);
     }
 
     // Update is called once per frame
