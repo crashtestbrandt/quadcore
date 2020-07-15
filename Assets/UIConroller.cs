33972b701deb44a72916bf45e49c60d2a909ab39
Brandt <starshipbrandt@gmail.com>
2020-07-15 16:20:43 -0500

    Adding new branch to work on player turn sequence

---
+++ Assets/UIConroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIConroller : MonoBehaviour
+{
+    public Canvas MainMenuCanvas;
+    public Canvas InfoCanvas; 
+    public Canvas SettingsCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
