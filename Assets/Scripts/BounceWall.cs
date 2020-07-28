781f3cc4b719b8291ad1caa864aa9a6b46520854
Brandt <starshipbrandt@gmail.com>
2020-07-27 20:08:03 -0500

    Fixed a stupid typo on master

--- Assets/Scripts/BounceWall.cs
+++ Assets/Scripts/BounceWall.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BouneWall : MonoBehaviour
+public class BounceWall : MonoBehaviour
 {
     public GameObject BounceEffectPrefab;
 
