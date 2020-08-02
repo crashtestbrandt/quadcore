d75c074c3c30bfe04f56283ef0f40a2e02032c38
Brandt <starshipbrandt@gmail.com>
2020-08-02 08:34:58 -0500

    Authenticated cloud function call demonstrated

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -46,7 +46,7 @@ public class NetworkController : MonoBehaviour
                 );
         });
         */
-
+        await Task.Delay(TimeSpan.FromSeconds(5.0f));
         Debug.Log("Attempting to call cloud function ...");
         functions = FirebaseFunctions.DefaultInstance;
 
@@ -61,32 +61,13 @@ public class NetworkController : MonoBehaviour
         dynamic result = await function.CallAsync(data);
         //var keys = result.Data.Keys;
         Debug.Log("Operation result: " + result.Data["operationResult"]);
-        //foreach (var key in keys)
-        //{
-        //    Debug.Log(key + " : " + result.Data[key]);
-        //}
-        /*
-        await addMessage("This is a fucking test.").ContinueWith((task) =>
-        {
-            if (task.IsFaulted)
-            {
-                foreach (var inner in task.Exception.InnerExceptions)
-                {
-                    if (inner is FunctionsException)
-                    {
-                        var e = (FunctionsException) inner;
-                        var code = e.ErrorCode;
-                        var message = e.Message;
-                    }
-                }
-            }
-            else
-            {
-                Debug.Log(task.Result);
-            }
-        }
-        );
-        */
+
+        data = new Dictionary<string, object>();
+        data["text"] = "This is a test message while authenticated!";
+        function = functions.GetHttpsCallable("addMessage");
+        result = await function.CallAsync(data);
+        Debug.Log("Write result: " + result.Data["writeResult"]);
+        Debug.Log("Original: " + result.Data["original"]);
 
     }
 /*
